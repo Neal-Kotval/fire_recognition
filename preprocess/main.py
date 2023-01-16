5ea769dc0577a85a7de67c67545a58899c6177f2
Neal-Kotval <neal.kotval@gmail.com>
2023-01-16 15:06:11 -0500

    stuff

---
+++ preprocess/main.py
@@ -0,0 +1,19 @@
+
+# import required module
+import os
+ 
+# iterate over files in
+# that directory
+i=0
+for filename in os.scandir("./fire_data/data/fire"):
+    if filename.is_file():
+        print(filename.path)
+        os.rename(str(filename.path), "./fire_data/data/fire/fire_nealkotval_" + str(i) + ".jpg")
+        i+=1
+
+i=0
+for filename2 in os.scandir("./fire_data/data/non_fire"):
+    if filename2.is_file():
+        print(filename2.path)
+        os.rename(str(filename2.path), "./fire_data/data/fire/non_fire_nealkotval_" + str(i) + ".jpg")
+        i+=1
