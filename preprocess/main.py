c4f426eb0edd136239dd5491dfe19be6f69fc5d5
Neal-Kotval <neal.kotval@gmail.com>
2023-01-16 15:42:03 -0500

    smash

--- preprocess/main.py
+++ preprocess/main.py
@@ -7,13 +7,17 @@ import os
 i=0
 for filename in os.scandir("./fire_data/data/fire"):
     if filename.is_file():
-        print(filename.path)
-        os.rename(str(filename.path), "./fire_data/data/fire/fire_nealkotval_" + str(i) + ".jpg")
+        # print(filename.path)
+        # os.rename(str(filename.path), "./fire_data/data/fire/fire_nealkotval_" + str(i) + ".jpg")
         i+=1
+
+print(i)
 
 i=0
 for filename2 in os.scandir("./fire_data/data/non_fire"):
     if filename2.is_file():
-        print(filename2.path)
-        os.rename(str(filename2.path), "./fire_data/data/non_fire/non_fire_nealkotval_" + str(i) + ".jpg")
+        # print(filename2.path)
+        # os.rename(str(filename2.path), "./fire_data/data/non_fire/non_fire_nealkotval_" + str(i) + ".jpg")
         i+=1
+
+print(i)
